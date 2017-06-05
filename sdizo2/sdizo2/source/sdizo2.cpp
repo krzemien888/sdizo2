4b0d1beddf1dd8a5816ad706f9f50b4e260219f8
Jakub Krzemiński <krzeminski.k@outlook.com>
2017-06-05 22:00:26 +0200

    Moved to app launching

--- sdizo2/sdizo2/source/sdizo2.cpp
+++ sdizo2/sdizo2/source/sdizo2.cpp
@@ -11,32 +11,8 @@
 
 int main()
 {
-	//AppController app;
+	AppController app;
 
-	listGraph m;
-
-
-	Point p1(0);
-	Point p2(1);
-	Point p3(2);
-	m.addPoint(p1);
-	m.addPoint(p2);
-	m.addPoint(p3);
-
-
-	Edge e1(1, p1, p2);
-	//Edge e2(1, p2, p3);
-	//Edge e3(1, p1, p3);
-
-	m.addEdge(e1);
-	//m.addEdge(e2);
-	//m.addEdge(e3);
-
-
-	m.print();
-
-	system("pause");
-	/*
 	try {
 		app.Run();
 	}
@@ -45,7 +21,7 @@ int main()
 		std::cout << "Critical error, application shutdown.\n";
 		system("pause");
 	}
-	*/
+	
     return 0;
 }
 
