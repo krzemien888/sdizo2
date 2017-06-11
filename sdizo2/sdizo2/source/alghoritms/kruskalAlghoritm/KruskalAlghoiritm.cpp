8b87482edd99fd99fe05e5d8b5b844e28f7b3dba
Jakub Krzemiński <krzeminski.k@outlook.com>
2017-06-11 21:24:54 +0200

    Kruskal alghoritm

--- sdizo2/sdizo2/source/alghoritms/kruskalAlghoritm/KruskalAlghoiritm.cpp
+++ sdizo2/sdizo2/source/alghoritms/kruskalAlghoritm/KruskalAlghoiritm.cpp
@@ -1,22 +1,36 @@
 #include "stdafx.h"
 #include "KruskalAlghoiritm.h"
-
-
-KruskalAlghoiritm::KruskalAlghoiritm()
-{
-}
-
-
-KruskalAlghoiritm::~KruskalAlghoiritm()
-{
-}
+#include "structures\priorityQueue\PriorityQueue.h"
+#include "structures\utilities\disjoinSet\DisjoinSet.h"
 
 void KruskalAlghoiritm::printResult()
 {
+	int graphWeight = 0;
+	cout << "Krawêdzie grafu rozpinaj¹cego: " << endl;
+	for (int i = 0; i < m_resultList.getSize(); i++)
+	{
+		cout << m_resultList.getValue(i) << endl;
+		graphWeight += m_resultList.getValue(i).getValue();
+	}
+	cout << "Suma wag krawêdzi: " << graphWeight << endl;
+
 }
 
 void KruskalAlghoiritm::apply(Graph * graph)
 {
+	DisjoinSet dj(m_currGraphSize);
+	for (int i = 0; i < m_currGraphSize; i++)
+		dj.makeSet(i);
+	
+	while (m_queue.getSize() != 0)
+	{
+		auto currEdge = m_queue.popElement();
+		if (dj.findSet(currEdge.getStartName()) !=  dj.findSet(currEdge.getEndName()))
+		{
+			dj.makeUnion(currEdge.getStartName(), currEdge.getEndName());
+			m_resultList.pushBack(currEdge);
+		}
+	}
 }
 
 void KruskalAlghoiritm::setParameters(int, int)
@@ -25,4 +39,11 @@ void KruskalAlghoiritm::setParameters(int, int)
 
 void KruskalAlghoiritm::prepare(Graph * graph)
 {
+	m_currGraphSize = graph->getAmountPoints();
+	m_resultList.clearStructure();
+	auto edges = graph->getEdges();
+	m_queue.clearStructure();
+
+	while (edges.getSize() != 0)
+		m_queue.addElement(edges.popFrontElement());
 }
--- sdizo2/sdizo2/source/alghoritms/kruskalAlghoritm/KruskalAlghoiritm.h
+++ sdizo2/sdizo2/source/alghoritms/kruskalAlghoritm/KruskalAlghoiritm.h
@@ -1,15 +1,21 @@
 #pragma once
 #include "alghoritms\IAlghoritm.h"
+#include "structures\priorityQueue\PriorityQueue.h"
+#include "structures\list\List.h"
+
 class KruskalAlghoiritm :
 	public IAlghoritm
 {
 public:
-	KruskalAlghoiritm();
-	~KruskalAlghoiritm();
+	KruskalAlghoiritm() = default;
+	~KruskalAlghoiritm() = default;
 
 	void printResult();
 	void apply(Graph* graph);
 	void setParameters(int, int);
 	void prepare(Graph* graph);
+private:
+	int m_currGraphSize = 0;
+	PriorityQueue<Edge> m_queue;
+	List<Edge> m_resultList;
 };
-
